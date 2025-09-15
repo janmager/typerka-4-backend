baf2d18d6069015264971b94a307eb31058290bd
Jan Mager <jasiu.mager@gmail.com>
2025-09-15 13:47:13 +0200

    cron update

--- src/config/cron.js
+++ src/config/cron.js
@@ -3,7 +3,7 @@ import https from "https";
 import { API_URL } from "./db.js";
 
 // for active state render server (going to sleep after 15min of disactive)
-export const wakeupJob = new cron.CronJob("*/1 * * * *", function () {
+export const wakeupJob = new cron.CronJob("*/14 * * * *", function () {
   const now = new Date();
   const gmtPlus2 = new Date(now.getTime() + (2 * 60 * 60 * 1000)); // GMT+2
   const timeString = `[${gmtPlus2.getHours().toString().padStart(2, '0')}:${gmtPlus2.getMinutes().toString().padStart(2, '0')} ${gmtPlus2.getDate().toString().padStart(2, '0')}.${(gmtPlus2.getMonth() + 1).toString().padStart(2, '0')}.${gmtPlus2.getFullYear()}]`;
