44e33c77761f4fd4dd415ff8910eb483cf48f3e0
Jan Mager <jasiu.mager@gmail.com>
2025-09-15 13:35:57 +0200

    fix

--- src/config/db.js
+++ src/config/db.js
@@ -4,5 +4,5 @@ import 'dotenv/config';
 // create a sql connection
 export const sql = neon(process.env.DATABASE_URL);
 
-export const API_URL = "http://localhost:5001";
-// export const API_URL = '';
+// export const API_URL = "http://localhost:5001";
+export const API_URL = 'https://typerka-4-backend.onrender.com';
